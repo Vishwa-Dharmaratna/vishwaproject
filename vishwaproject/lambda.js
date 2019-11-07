99b91795202aa88f36f7bc370b5365ebbabafa10
vishwa96vishu <smvishwadharmaratna96@gmail.com>
2019-11-07 13:15:41 +0530

    added dp

--- vishwaproject/lambda.js
+++ vishwaproject/lambda.js
@@ -1,7 +1,8 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
     let s3 = event.Records[0].s3;
@@ -14,9 +15,24 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+
+             let lbl = data.Labels[0];
+            console.log(lbl.Name)
+            ddb.put({
+                TableName: 'word',
+                Item: { 'name': s3.object.key, 'lable': lbl.name }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                });
+            console.log(data);
+
+
+            callback(null, {});
+        })
+        .catch(err => callback(err));
 }
